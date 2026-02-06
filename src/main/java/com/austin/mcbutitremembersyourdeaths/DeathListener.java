60cb8dd48bd7c6e9093fbd4502efbea1a8d9d83d
Austin McDonald <austinmcdonald2011@gmail.com>
2026-02-06 12:25:07 -0500

    Deleted unused code

--- src/main/java/com/austin/mcbutitremembersyourdeaths/DeathListener.java
+++ src/main/java/com/austin/mcbutitremembersyourdeaths/DeathListener.java
@@ -1,79 +1,3 @@
-// package com.austin.mcbutitremembersyourdeaths;
-
-// import java.util.Random;
-
-// import net.minecraft.network.chat.Component;
-// import net.minecraft.world.entity.player.Player;
-// import net.minecraftforge.event.entity.living.LivingDeathEvent;
-// import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
-// import net.minecraftforge.fml.common.Mod;
-
-
-
-// @Mod.EventBusSubscriber(
-//         modid = "mcbutitremembersyourdeaths",
-//         bus = Mod.EventBusSubscriber.Bus.FORGE
-// )
-// public class DeathListener {
-
-//     private static final Random random = new Random();
-
-//     private static final String[] fallMessages = {
-//             "You fell from a high place… again.",
-//             "Gravity called. You didn’t pick up.",
-//             "Careful with those ledges, huh?"
-//     };
-
-//     private static final String[] fireMessages = {
-//             "Hot enough for you?",
-//             "Someone call the fire department!",
-//             "Do not try this at home… or anywhere."
-//     };
-
-//     private static final String[] lavaMessages = {
-//             "You’re really heating things up… literally.",
-//             "Molten death is always stylish.",
-//             "Lava: 1, You: 0."
-//     };
-
-//     private static final String[] genericMessages = {
-//             "Ouch! That looked painful.",
-//             "Seriously? That again?",
-//             "Try not to die in the same way twice!"
-//     };
-//         @SubscribeEvent
-// public static void onPlayerDeath(LivingDeathEvent event) {
-//     if (!(event.getEntity() instanceof Player player)) return;
-//     if (player.level().isClientSide()) return;
-
-//     String type = event.getSource().getMsgId();
-//     String message = getFunnyMessage(type);
-
-//     player.displayClientMessage(
-//         Component.literal(message),
-//         false
-//     );
-// }
-//     private static String getFunnyMessage(String type) {
-//         if (type == null) return randomFrom(genericMessages);
-
-//         return switch (type) {
-//             case "fall" -> randomFrom(fallMessages);
-//             case "fire", "onFire" -> randomFrom(fireMessages);
-//             case "lava" -> randomFrom(lavaMessages);
-//             case "explosion", "explosion.player", "explosion.mob" ->
-//                     "Boom! That didn’t go well.";
-//             case "magic" -> "Spell fail! Try again.";
-//             case "arrow", "thrown" ->
-//                     "Ouch! That projectile had your name on it.";
-//             default -> randomFrom(genericMessages);
-//         };
-//     }
-
-//     private static String randomFrom(String[] arr) {
-//         return arr[random.nextInt(arr.length)];
-//     }
-// }
 package com.austin.mcbutitremembersyourdeaths;
 
 import java.util.HashMap;
@@ -94,7 +18,6 @@ public class DeathListener {
 
     private static final Random random = new Random();
 
-    // Keep track of deaths: player UUID + damage type -> count
     private static final Map<String, Integer> deathCounts = new HashMap<>();
 
     private static final String[] fallMessages = {
